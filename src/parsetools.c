ec46116e4e7b28a794d848607eefdf9e6685cc5b
Ellendren <54527908+Ellendren@users.noreply.github.com>
2020-11-24 23:57:18 -0800

    Update parsetools.c

--- src/parsetools.c
+++ src/parsetools.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "constants.h"
 #include "parsetools.h"
@@ -57,7 +58,6 @@ char* removeSpaces(char* str){
 }
 
 //executeData struct will hold info to execute command, numOfCmds will hold the number of commands that are parsed
-//cmds holds the splut commands
 void Parse(char ** cmds,struct executeData *parseCmd, int numOfCmds){
     int totalCmdSize =0;
 
@@ -67,36 +67,17 @@ void Parse(char ** cmds,struct executeData *parseCmd, int numOfCmds){
             parseCmd[i].cmdWrds[j] = (char *)malloc(MAX_LINE_WORDS * sizeof(char));
         }
 
-
         //need to recognize and remove spaces
         cmds[i] = removeSpaces(cmds[i]);
         //removed white spaces and will need to store commands into the struct array
         totalCmdSize =split_cmd_line(cmds[i],parseCmd[i].cmdWrds);
-
-        for(int j =0; j < totalCmdSize; j++){
-            //compare strings, the strcmp will compare both strings and if both are identical then it will return 0.
-            //Will return a + or - number depending if what string is greater or less than the other
-            if(strcmp(parseCmd[i].cmdWrds[j],">")){
-                //will be used as a condition in the exec in main.
-                parseCmd[i].out =1;
-
-                //copy contents from parseCmd[i].cmdWrds[j+1] into the out_file array
-                strcpy(parseCmd[i].out_File,parseCmd[i].cmdWrds[j+1]);
-
-                //decrease commands size by 2, because of the copy
-                totalCmdSize-=2;
-
-                //null the rest of the array
-                parseCmd[i].cmdWrds[j] = '\0';
-                parseCmd[i].cmdWrds[j+1] = '\0';
-            }
-
-        }
 
     }
 }
 
 
 //function to remove quotes
+
+
 
 
