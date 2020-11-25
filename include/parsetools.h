01f4d59424fe7953695cac167c17b2b19026e659
Chris <chrisware707@gmail.com>
2020-11-25 15:19:44 -0800

    Cleaned-up and fixed first two test

--- include/parsetools.h
+++ include/parsetools.h
@@ -10,8 +10,10 @@
 //
 int split_cmd_line(char* line, char** list_to_populate);
 int split_line_pipes(char* line, char** list_to_populate);
-void exec(struct executeData *parseCmd,int numOfCmds);
 void Parse(char ** cmds,struct executeData *parseCmd,int numOfCmds);
 char* removeSpaces(char* str);
+void exec(struct executeData *parseCmd,int numOfCmds);
+void execPipe(struct executeData *parseCmd,int numOfCmds);
+void syserror(const char *);
 
 #endif
--- src/parsetools.c
+++ src/parsetools.c
@@ -3,11 +3,14 @@
 #include <string.h>
 #include "constants.h"
 #include "parsetools.h"
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
 
 
 
 // Parse a command line into a list of words,
-//    separated by whitespace.
+// Separated by whitespace.
 // Returns the number of words
 //
 int split_cmd_line(char* line, char** list_to_populate) {
@@ -40,7 +43,7 @@ int split_line_pipes(char* line, char** list_to_populate) {
 }
 
 //since the split cmd line function will accept s string (char *) we need to remove leading plus trailing whitespaces from the string
-char* removeSpaces(char* str){
+char* removeSpaces(char* str) {
     char* temp;
     //trim leading
     while(*str == ' ')
@@ -59,20 +62,110 @@ char* removeSpaces(char* str){
 
 //executeData struct will hold info to execute command, numOfCmds will hold the number of commands that are parsed
 void Parse(char ** cmds,struct executeData *parseCmd, int numOfCmds){
-    int totalCmdSize =0;
+    int totalCmdSize = 0;
 
     // allocate memory for struct array executeData
     for(int i =0; i < numOfCmds; i++) {
         for (int j = 0; j < MAX_LINE_WORDS + 1; j++) {
             parseCmd[i].cmdWrds[j] = (char *)malloc(MAX_LINE_WORDS * sizeof(char));
         }
-
         //need to recognize and remove spaces
         cmds[i] = removeSpaces(cmds[i]);
         //removed white spaces and will need to store commands into the struct array
-        totalCmdSize =split_cmd_line(cmds[i],parseCmd[i].cmdWrds);
-
+        totalCmdSize = split_cmd_line(cmds[i],parseCmd[i].cmdWrds);
     }
+}
+
+//execute command lines arguments
+void exec(struct executeData *parseCmd,int numOfCmds) {
+    pid_t pid;
+
+    //one input
+    if(numOfCmds == 1){
+        pid =fork();
+        if(pid == -1){
+            syserror("Not able to create a fork");
+        }
+        else if(pid == 0){
+            execvp(parseCmd[0].cmdWrds[0], parseCmd[0].cmdWrds);
+            exit(0);
+        }
+        else{
+            pid = wait(NULL);
+        }
+    }
+    else{
+        execPipe(parseCmd, numOfCmds);
+    }
+}
+
+//executes piped commands
+void execPipe(struct executeData *parseCmd,int numOfCmds) {
+    int fds[numOfCmds-1][2];
+    pid_t pid;
+
+    //first make all the needed pipes
+    for(int i = 0; i < numOfCmds - 1; i++){
+        if (pipe(fds[i]) == -1)
+            syserror( "Could not create a pipe" );
+    }
+
+    //then exec them
+    for(int i = 0; i < numOfCmds - 1; i++) {
+
+        switch ( pid = fork() ) {
+            case -1:
+                syserror("First fork failed");
+                break;
+            case  0:
+                if (close(0) == -1)
+                    syserror("Could not close stdin");
+                dup(fds[i][0]);
+                if (close(fds[i][0]) == -1 || close(fds[i][1]) == -1)
+                    syserror( "Could not close fds[i]s from first child" );
+                execvp(parseCmd[i+1].cmdWrds[0], parseCmd[i+1].cmdWrds);
+                syserror("Could not exec ");
+                break;
+            default:
+                //fprintf(stderr, "The first child's pid is: %d\n", pid);
+                break;
+        }
+
+        switch ( pid = fork() ) {
+            case -1:
+                syserror( "Second fork failed" );
+                break;
+            case  0:
+                if (close(1) == -1)
+                    syserror("Could not close stdout");
+                dup(fds[i][1]);
+                if (close(fds[i][0]) == -1 || close(fds[i][1]) == -1)
+                    syserror( "Could not close fds[i]s from first child" );
+                execvp(parseCmd[i].cmdWrds[0], parseCmd[i].cmdWrds);
+                syserror("Could not exec ");
+                break;
+            default:
+                //fprintf(stderr, "The second child's pid is: %d\n", pid);
+                break;
+        }
+    }
+    //finally reap children
+    for(int i = 0; i < numOfCmds - 1; i++) {
+        if (close(fds[i][0]) == -1)
+            syserror( "Parent could not close stdin" );
+        if (close(fds[i][1]) == -1)
+            syserror( "Parent could not close stdout" );
+
+        while ( wait(NULL) != -1);
+    }
+}
+
+//check errors for function calls, got from pipe_demo
+void syserror(const char *s) {
+    extern int errno;
+    fprintf(stderr, "%s\n", s);
+    fprintf(stderr, " (%s)\n", strerror(errno));
+    exit(1);
 }
 
 
