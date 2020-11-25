7d1079460a35671bcc1353b8499456ff15ec6b9c
Ellendren <54527908+Ellendren@users.noreply.github.com>
2020-11-24 23:56:33 -0800

    Update main.c

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include "parsetools.h"
 
 void exec(struct executeData *parseCmd,int numOfCmds);
+void execPipe(struct executeData *parseCmd,int numOfCmds);
 void syserror(const char *);
 
 int main()
@@ -18,75 +19,129 @@ int main()
     char line[MAX_LINE_CHARS];
     // holds separated words based on whitespace
     char* line_words[MAX_LINE_WORDS + 1];
-
+    
     // Loop until user hits Ctrl-D (end of input)
     // or some other input error occurs
     while( fgets(line, MAX_LINE_CHARS, stdin) ) {
-        int num_words = split_cmd_line(line, line_words); //probably splits the command line for pipes
+        if(line[0] == EOF)//exit if ctrl-d is entered
+            break;
+        
+        int numOfCmds = split_line_pipes(line, line_words); //probably splits the command line for pipes
 
-        //int numOfCmds =split_cmd_line(line,line_words); //split command line
+    //I don't think we need this line, Parse handles this --Luke
+    //    int numOfCmds = split_cmd_line(line,line_words); //split command line
 
-        struct executeData parseCmd[num_words]; //store command from parse
+        struct executeData parseCmd[numOfCmds]; //store command from parse
 
-        Parse(line_words,parseCmd,num_words); //parse commands pass to exec
+        Parse(line_words,parseCmd,numOfCmds); //parse commands pass to exec
 
-        exec(parseCmd,num_words);
+/*
+        int pid = fork();
+        if (pid == 0) {
+            printf("number of commands: %d\n", numOfCmds);
+            execvp(parseCmd[0].cmdWrds[0], parseCmd[0].cmdWrds);
+            exit(0);
+        }else {
+            pid = wait(NULL);
+        }*/
+        
+        exec(parseCmd, numOfCmds);
 
-        for (int i=0; i < num_words; i++) {
-            printf("%s\n", line_words[i]);
-        }
+//        for (int i = 0; i < num_words; i++) {
+//            printf("%s\n", line_words[i]);
+//        }
     }
+    printf("\n");
 
     return 0;
 }
 
 //execute command lines arguments
 void exec(struct executeData *parseCmd,int numOfCmds){
+    pid_t pid;
+
+    //one input
+    if(numOfCmds == 1){
+        pid =fork();
+        if(pid == -1){
+            syserror("Not able to create a fork");
+        }
+        else if(pid == 0){
+            execvp(parseCmd[0].cmdWrds[0], parseCmd[0].cmdWrds);
+            exit(0);
+        }
+        else{
+            pid = wait(NULL);
+        }
+    }
+    else{
+        execPipe(parseCmd, numOfCmds);
+    }
+}
+
+
+//executes piped commands
+void execPipe(struct executeData *parseCmd,int numOfCmds){
     int fds[numOfCmds-1][2];
     pid_t pid;
 
-    if (pipe(fds) == -1)
-        syserror( "Could not create a pipe" );
-
-    //one input, tried redirection
-    if(numOfCmds == 1){
-        pid = fork();
-        if(pid == -1){
-            syserror("Not able to fork command");
-        }
-        if(pid == 0){
-            //check in or out redirection
-            if(parseCmd[0].out || parseCmd[0].in){
-                if(parseCmd[0].out == 1){
-                    //create new file description and set the return value to fds(lowest number of the file descriptor)
-                    fds[0][1] = open(parseCmd[0].out_File,O_WRONLY | O_CREAT);
-                    dup2(fds[0][1], 1);
-                }
-
-            }
-            execlp(parseCmd[0].cmdWrds[0], parseCmd[0].cmdWrds);
-            //syserror("Could not exec the command");
-
-        }
+    //first make all the needed pipes
+    for(int i = 0; i < numOfCmds - 1; i++){
+        if (pipe(fds[i]) == -1)
+            syserror( "Could not create a pipe" );
     }
 
-    for(int i =0; i < numOfCmds; i++){
-        switch(pid == fork){
-            case -1:
-                syserror("First fork failed");
+    //then exec them
+    for(int i = 0; i < numOfCmds - 1; i++){
+
+        switch ( pid = fork() ) {
+        case -1: 
+            syserror("First fork failed");
+            break;
+        case  0: 
+            if (close(0) == -1)
+                syserror("Could not close stdin");
+            dup(fds[i][0]);
+            if (close(fds[i][0]) == -1 || close(fds[i][1]) == -1)
+                syserror( "Could not close fds[i]s from first child" );
+            execvp(parseCmd[i+1].cmdWrds[0], parseCmd[i+1].cmdWrds);
+            syserror("Could not exec ");
+            break;
+        default:
+            //fprintf(stderr, "The first child's pid is: %d\n", pid);
+            break;
+        }
+
+        switch ( pid = fork() ) {
+            case -1: 
+                syserror( "Second fork failed" );
                 break;
-            case 0:
-                dup2(fds[i][0],1);
-                if (close(fds[0]) == -1 || close(fds[1]) == -1)
-                    syserror( "Could not close fds from first child" );
-
-                execlp(parseCmd[0].cmdWrds[0], parseCmd[0].cmdWrds);
-                //syserror("Could not exec the command");
-
+            case  0: 
+                if (close(1) == -1)
+                    syserror("Could not close stdout");
+                dup(fds[i][1]);
+                if (close(fds[i][0]) == -1 || close(fds[i][1]) == -1)
+                    syserror( "Could not close fds[i]s from first child" );
+                execvp(parseCmd[i].cmdWrds[0], parseCmd[i].cmdWrds);
+                syserror("Could not exec ");
+                break;
             default:
-                fprintf(stderr, "The first child's pid is: %d\n", pid);
+                //fprintf(stderr, "The second child's pid is: %d\n", pid);
                 break;
         }
+        
+        
+    }
+
+    //finaly reap children
+    for(int i = 0; i < numOfCmds - 1; i++){
+        if (close(fds[i][0]) == -1)
+            syserror( "Parent could not close stdin" );
+        if (close(fds[i][1]) == -1)
+            syserror( "Parent could not close stdout" );
+
+        while ( wait(NULL) != -1)
+            ;
     }
 
 }
@@ -101,3 +156,4 @@ void syserror(const char *s)
     fprintf(stderr, " (%s)\n", strerror(errno));
     exit(1);
 }
+
